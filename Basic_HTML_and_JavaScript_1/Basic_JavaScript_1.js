c6c2fccecc264de8faeb67b003765406e495c29b
ahart8 <jedimaster1971@hotmail.com>
2023-01-31 20:39:26 -0500

    Reorganized repo structure

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,10 @@
+alert("Hello, World! I am the alert text box!");
+
+	
+function My_First_Function(){
+	var String="Kiss Me, I'm Irish";
+	var result= String.fontcolor("green")
+	document.getElementById("Irish").innerHTML=
+	result;
+	
+}
---
+++ Basic_HTML_and_JavaScript_1/README.md
@@ -0,0 +1,3 @@
+# Basic_HTML_and_JavaScript_1
+This is where I will be putting my HTML and JavaScript code assignments
+I love 💻
